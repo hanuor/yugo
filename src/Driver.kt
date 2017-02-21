e563d293c906e9d2678d74efc5ec430d5b6c55ae
hanuor <hanuortech@gmail.com>
2017-02-21 10:16:51 +0530

    Initialize

---
+++ src/Driver.kt
@@ -0,0 +1,6 @@
+/**
+ * Created by Shantanu Johri on 21-02-2017.
+ */
+object Driver{
+
+}
--- src/Perceptron.kt
+++ src/Perceptron.kt
@@ -3,4 +3,14 @@
  */
 fun main(args: Array<String>){
     println("Kotlin ini")
+    val data = arrayOf(
+            arrayOf(intArrayOf(0, 0), intArrayOf(0)),
+            arrayOf(intArrayOf(0, 1), intArrayOf(0)),
+            arrayOf(intArrayOf(1, 0), intArrayOf(0)),
+            arrayOf(intArrayOf(1, 1), intArrayOf(1))
+    )
+    val LEARNINGRATE : Double = 0.05
+    val INITIAL_WEIGHTS: DoubleArray = doubleArrayOf(Math.random(), Math.random())
+
+    
 }
